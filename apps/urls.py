3f3dd002459e3dcdba54da3950d62ed6540708a5
krv006 <rvkamronbek@gmail.com>
2024-08-21 14:47:28 +0500

    change

--- apps/urls.py
+++ apps/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+
+from apps.views import HomeView
+
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
 ]
--- root/settings.py
+++ root/settings.py
@@ -96,11 +96,9 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

afc0ec78e40ad25c0a50a37435a1dd8c5472a7f8
krv006 <rvkamronbek@gmail.com>
2024-08-23 11:32:56 +0500

    change

--- apps/views.py
+++ apps/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView
+from django.db.models import Max
+from django.views.generic import ListView, DetailView
 
-from apps.models import Category, Product, ImageProduct
+from apps.models import Category, Product
 
 
 # Create your views here.
@@ -11,12 +11,13 @@ class HomeListView(ListView):
     template_name = 'apps/product/home.html'
     context_object_name = 'categories'
 
-    # def get_image(self, category):
-    #     if category.image:
-    #         print(category.image)
-    #         return category.image.url
-    #     else:
-    #         pass
+    def get_queryset(self):
+        latest_products = Product.objects.all().order_by('-created_at')[:8]
+        qs = Category.objects.filter(products__in=latest_products).distinct()
+        qs = qs.annotate(latest_product_date=Max('products__created_at'))
+        ordering = self.request.GET.get('ordering', '-latest_product_date')
+        qs = qs.order_by(ordering)
+        return qs
 
 
 class ProductListView(ListView):
@@ -30,10 +31,17 @@ class ProductListView(ListView):
         if image:
             return image.image.url
         print(image)
-        return '/path/to/default/image.jpg'  # Replace with your actual default image path
+        return '/path/to/default/image.jpg'
 
 
 class ProductDetailDetailView(DetailView):
     queryset = Product.objects.all()
     template_name = 'apps/product/product-detail.html'
     context_object_name = 'product'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        if ordering := self.request.GET.get('-ordering'):
+            qs = qs.order_by(ordering)
+        return qs
